ded7aee409ca92c8fb8f028661230c211e1304d1
Devin Beliveau <devinmarieb@gmail.com>
2016-08-30 17:17:59 -0600

    added some notes to what needs to be done.

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,7 @@ var inputTitle;
 var inputLink;
 var read = '<td><input class="read-check" type="checkbox"></td>';
 var remove = '<td><button class="remove" type="button" name="remove"> Remove </button> </td>';
+var unRead;
 
 $('#submit-button').on('click', function() {
   inputTitle = $('#input-title').val();
@@ -12,8 +13,13 @@ $('#submit-button').on('click', function() {
     '</td>'+ read + remove +'</tr>');
   $('.remove').on('click', function() {
     $(this).parents('tr').remove();
-  $('.read-check').on ('click', function() {
-    $(this).addClass('.read');
+  $('.read-check').on('click', function() {
+    $(this).addClass('read');
     });
   });
 });
+
+//need to get the input field to convert the text to links
+//need to toggle the classes (when box is unchecked it still has class of read)
+//need to make the button functions work outside of the #submit-button
+//need to get the first row to not show up on page load

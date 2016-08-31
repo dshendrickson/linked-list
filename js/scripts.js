c0edafcbab447a99d25de647718ec6bacd6cffe1
Devin Beliveau <devinmarieb@gmail.com>
2016-08-30 21:37:35 -0600

    added remove button functionality with event delegator.

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,26 @@ var inputTitle;
 var inputLink;
 var read = '<td><input class="read-check" type="checkbox"></td>';
 var remove = '<td><button class="remove" type="button" name="remove"> Remove </button> </td>';
-var unRead;
+// var unRead;
 
 $('#submit-button').on('click', function() {
   inputTitle = $('#input-title').val();
   inputLink = $('#input-link').val();
   $('.link-list tr:last').after
     (`<tr><td>${inputTitle}
-    </td><td><a href='https://${inputLink}'>link</a>
+    </td><td><a href='https://${inputLink}'></a>
     </td> ${read} ${remove}</tr>`);
-  $('.remove').on('click', function() {
-    $(this).parents('tr').remove();
-  $('.read-check').on('click', function() {
-    $(this).addClass('read');
-    });
-  });
 });
 
-//need to toggle the classes (when box is unchecked it still has class of read)
-//need to make the button functions work outside of the #submit-button
+$('table').delegate('td', 'click', function () {
+ $(this).toggleClass('read', 'unread');
+});
+
+$('table').delegate('.remove', 'click', function () {
+  $(this).parents('tr').remove();
+});
+
+
+//delegates work when clicked anywhere on the row not just the box/button
+//the class of read/uread is only applied to the td element
 //need to get the first row to not show up on page load

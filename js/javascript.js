8fb7c07631309460bf40f7569c379a190ea70a20
Devin Beliveau <devinmarieb@gmail.com>
2016-08-30 18:07:46 -0600

    added link functionality.

--- js/javascript.js
+++ js/javascript.js
@@ -8,9 +8,9 @@ $('#submit-button').on('click', function() {
   inputTitle = $('#input-title').val();
   inputLink = $('#input-link').val();
   $('.link-list tr:last').after
-    ('<tr><td>'+ inputTitle +
-    '</td><td>'+ inputLink +
-    '</td>'+ read + remove +'</tr>');
+    (`<tr><td>${inputTitle}
+    </td><td><a href='https://${inputLink}'>link</a>
+    </td> ${read} ${remove}</tr>`);
   $('.remove').on('click', function() {
     $(this).parents('tr').remove();
   $('.read-check').on('click', function() {
@@ -19,7 +19,6 @@ $('#submit-button').on('click', function() {
   });
 });
 
-//need to get the input field to convert the text to links
 //need to toggle the classes (when box is unchecked it still has class of read)
 //need to make the button functions work outside of the #submit-button
 //need to get the first row to not show up on page load

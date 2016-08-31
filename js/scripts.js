e9d3a9c517fac7f677c19a8ecf6f566f1e974143
Dale S Hendrickson <dale@dshendrickson.com>
2016-08-31 15:15:12 -0600

    Added submit button enable/disble functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+var inputTitle = $('#input-title');
+var inputLink;
 var read = '<td><button class="read-check" type="button" name="read">Read</td>';
 var remove = '<td><button class="remove" type="button" name="remove"> Remove </button> </td>';
+
+
+$('#submit-button').prop('disabled', true);
+
+function toggleSubmitButton () {
+  if($('#input-title').val() != '' && $('#input-link').val() != '') {
+    $('#submit-button').prop('disabled', false);
+  } else {
+    $('#submit-button').prop('disabled', true);
+  }
+};
+
+$(inputTitle).keyup(function() {
+  toggleSubmitButton();
+});
+
+$('#input-link').keyup(function() {
+  toggleSubmitButton();
+});
 
 $('#submit-button').on('click', function() {
 var inputTitle = $('#input-title').val();

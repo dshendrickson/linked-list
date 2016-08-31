5114423da48e7a4a9534977ab173eca1b230f18f
Devin Beliveau <devinmarieb@gmail.com>
2016-08-31 13:16:24 -0600

    added alert when fields are empty.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@ var inputTitle;
 var inputLink;
 var read = '<td><button class="read-check" type="button" name="read">Read</td>';
 var remove = '<td><button class="remove" type="button" name="remove"> Remove </button> </td>';
-var removeButton = document.querySelector('#submit-button');
-// var unRead;
 
 
 $('#submit-button').on('click', function() {
   inputTitle = $('#input-title').val();
   inputLink = $('#input-link').val();
-  $('.link-list tr:last').after
-    (`<tr><td>${inputTitle}
-    </td><td><a href='https://${inputLink}'>${inputLink}</a>
-    </td> ${read} ${remove}</tr>`);
+    if (inputTitle === '' || inputLink === '') {
+    alert('please enter information');
+      } else {
+        $('.link-list tr:last').after
+        (`<tr><td>${inputTitle}
+        </td><td><a href='https://${inputLink}'>${inputLink}</a>
+        </td> ${read} ${remove}</tr>`);
+  };
 });
 
 $('table').on('click', '.read-check', function () {
@@ -24,11 +26,4 @@ $('table').on('click', '.remove', function () {
 });
 
 
-
-
-
-//links show up as read when adding a new row
-//delegates work when clicked anywhere on the row not just the box/button
-//the class of read/uread is only applied to the td element
-//get the url to show up
-//need to get the first row to not show up on page load
+//make site responsive
